ccf3c12cedd227af25d50c466832443c779dca3f
Dmitry Pigin <dpigin@cordial.com>
2023-12-07 16:24:11 +0200

    Added validation to check if all ShardingConfigurationSource properties are correctly initialized before proceeding with loadConfiguration()

--- src/ShardingConfigurationSource.php
+++ src/ShardingConfigurationSource.php
@@ -5,6 +5,7 @@ namespace PHPLRPM\Cluster;
 use PHPLRPM\Cluster\Filters\ShardingConfigurationFilter;
 use PHPLRPM\Cluster\Providers\ClusterConfigurationProvider;
 use PHPLRPM\ConfigurationSource;
+use RuntimeException;
 
 abstract class ShardingConfigurationSource implements ConfigurationSource
 {
@@ -26,6 +27,7 @@ abstract class ShardingConfigurationSource implements ConfigurationSource
 
     public function loadConfiguration(): array
     {
+        $this->validateInitializedProperties();
         $inputConfig = $this->confSource->loadConfiguration();
         $clusterConfig = $this->clusterConfProvider->loadClusterConfiguration();
         return $this->filterConfig($inputConfig, $clusterConfig);
@@ -34,5 +36,32 @@ abstract class ShardingConfigurationSource implements ConfigurationSource
     private function filterConfig(array $inputConfig, ClusterConfiguration $clusterConf): array
     {
         return $this->configurationFilter->filterConfig($inputConfig, $clusterConf);
+    }
+
+    private function validateInitializedProperties(): void
+    {
+        if (!(isset($this->confSource))
+            || !($this->confSource instanceof ConfigurationSource)
+        ) {
+            throw new RuntimeException(
+                'ShardingConfigurationSource::confSource must be initialized and implement ConfigurationSource'
+            );
+        }
+
+        if (!(isset($this->clusterConfProvider))
+            || !($this->clusterConfProvider instanceof ClusterConfigurationProvider)
+        ) {
+            throw new RuntimeException(
+                'ShardingConfigurationSource::clusterConfProvider must be initialized and implement ClusterConfigurationProvider'
+            );
+        }
+
+        if (!(isset($this->configurationFilter))
+            || !($this->configurationFilter instanceof ShardingConfigurationFilter)
+        ) {
+            throw new RuntimeException(
+                'ShardingConfigurationSource::configurationFilter must be initialized and implement ShardingConfigurationFilter'
+            );
+        }
     }
 }
